df6ff6b950041844bfed75011d7dbfe6983efa90
Mahmoud <mhd-10@hotmail.com>
2019-02-14 12:44:06 +0300

    update

--- kittens/routes/index.js
+++ kittens/routes/index.js
@@ -1,21 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var unirest = require('unirest')
+var cats = require("cats-js");
+
+
+async function getCats(n,type){
+  let catsUrls = []
+  var c = new cats();
+  let catss = await c.get({results_per_page: n, type: type})
+  for ( let i of catss.images.image){
+    catsUrls.push(i.url)
+  }
+ // console.log(catss.images.image)
+  return catsUrls
+}
 
 
 
 
 
-unirest.get("https://community-placekitten.p.rapidapi.com/200/300")
-.header("X-RapidAPI-Key", "75c4ef894amsh275ed667a017d9fp15c968jsn8195bd911b7f")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+// unirest.get("https://community-placekitten.p.rapidapi.com/200/300")
+// .header("X-RapidAPI-Key", "75c4ef894amsh275ed667a017d9fp15c968jsn8195bd911b7f")
+// .end(function (result) {
+ 
+// });
+
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  urls = await getCats(100,"gif")
+  res.render('index', { title: 'cats' , urls: urls});
 });
 
 module.exports = router;
