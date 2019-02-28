e1940e004e623313d75668e7d16cfeb3cc36b2a3
ubuntu <fatmafouady89@gmail.com>
2019-02-28 00:59:48 -0800

    update

--- shoppingcart/bin/mouduls/product.js
+++ shoppingcart/bin/mouduls/product.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+
+
+var shcema = new Schema({
+
+
+
+imagePath : {type : String, required:true}
+
+})
+module.exports= mongoose.module('product', schema)
---
+++ shoppingcart/routes/seed/productseed.js
@@ -0,0 +1,6 @@
+var Product = require ('../mouduls/product');
+var product = new Product ({
+
+imagePath: 'https://www.google.com.sa/search?q=images&tbm=isch&source=iu&ictx=1&fir=A6JJqffgz3xzlM%253A%252CpFs_4Fcq5AgpmM%252C_&usg=AI4_-kSe7kutL7fo50LZBJwVxdtPCks5eQ&sa=X&ved=2ahUKEwiF95qxwdzgAhUhgM4BHeJJDc8Q9QEwAXoECAUQBA#imgrc=A6JJqffgz3xzlM:'
+
+});
