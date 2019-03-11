5c51d1dea415f476af749fe756afe84a61c6583d
Mahmoud <mhd-10@hotmail.com>
2019-03-11 13:34:54 +0300

    y

--- notes/api/routes/mynotes.js
+++ notes/api/routes/mynotes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mynotes', { title: 'mynotes' });
+  res.render('mynotes', { title: 'mynotes',buttons:[{title:"register",url:"/register/"}, {title:"mynotes",url:"/mynotes"} ]  });
 });
 
 
--- notes/api/routes/register.js
+++ notes/api/routes/register.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'register' });
+  res.render('register', { title: 'register' ,buttons:[{title:"register",url:"/register/"}, {title:"mynotes",url:"/mynotes"} ] });
 });
 
 
