ddf5cb89054ecf3ed733db7f5eaa66a4f73cd32d
ubuntu <fatmafouady89@gmail.com>
2019-05-07 03:15:32 -0700

    done

--- cantstoplearning/note-app/app.js
+++ cantstoplearning/note-app/app.js
@@ -1,6 +1,10 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-//fs.writeFileSync('note.txt','my name is fatma' )
+// //fs.writeFileSync('note.txt','my name is fatma' )
 
 
-fs.appendFileSync('note.txt', ' i live on kuwait')
+// fs.appendFileSync('note.txt', ' i live on kuwait')
+
+    require('./utils.js')
+    const name =' fatma'
+    console.log(name)
